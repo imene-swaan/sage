702c3d6323b2bb7ca628adc752ab710766b108f0
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2024-01-09 21:57:37 +0100

    first commit

---
+++ sage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sage
+//
+//  Created by Imene Kolli on 09.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Moni!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sage/sageApp.swift
@@ -0,0 +1,17 @@
+//
+//  sageApp.swift
+//  sage
+//
+//  Created by Imene Kolli on 09.01.24.
+//
+
+import SwiftUI
+
+@main
+struct sageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
