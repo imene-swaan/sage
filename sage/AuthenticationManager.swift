73644456b0219aa7a6e1edd4273838d4db82c935
aymanehachcham <aymanehachchaming@gmail.com>
2024-01-12 22:30:09 +0100

    Login view refactorised

---
+++ sage/AuthenticationManager.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationManager.swift
+//  sage
+//
+//  Created by Aymane Hachcham on 12/01/2024.
+//
+
+import Foundation
--- sage/HomeView.swift
+++ sage/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  sage
 //
 //  Created by Aymane Hachcham on 12/01/2024.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ContentView()
+    HomeView()
 }
